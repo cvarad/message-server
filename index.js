ce3e65f54e7a36552d7c5102bf7efb1fdf903b49
Varad Raut <varadraut@gmail.com>
2022-04-16 22:40:04 -0600

    Added basic message parsing, logging and routing

--- index.js
+++ index.js
@@ -2,21 +2,28 @@ const https = require('https');
 const fs = require('fs');
 const WebSocket = require('ws');
 
+const parser = require('./parser');
+const logger = require('./logger').getConsoleLogger(' MAIN ');
+
+
 const server = https.createServer({
     key: fs.readFileSync('key.pem'),
     cert: fs.readFileSync('cert.pem')
-}, (req, res) => {
+}, (_, res) => {
     res.writeHead(200);
-    res.end('Hello there! This is a temporary page to let you download the tls certificate and add it to the trusted root CAs');
+    res.end('Hello! Download the TLS certificate and add it to the trusted root CAs (If not done already)!');
 });
-const wss = new WebSocket.Server({server});
+const wss = new WebSocket.Server({ server });
 
 let convUserMap = new Map();
-function addConversation(conversationId, sender, receiver) {
+function addConversation(conversationId, from, to) {
     if (convUserMap.has(conversationId)) {
         return;
     }
-    convUserMap.set(conversationId, [sender, receiver]);
+    convUserMap.set(conversationId, [from, to]);
+}
+function getConvParticipants(conversationId) {
+    return convUserMap.get(conversationId);
 }
 
 let userSockMap = new Map();
@@ -26,20 +33,84 @@ function addUser(userId, ws) {
     }
     userSockMap.set(userId, ws);
 }
+function getUserSocket(userId) {
+    return userSockMap.get(userId);
+}
+function removeUser(userId) {
+    userSockMap.delete(userId);
+}
 
-wss.on('connection', (ws) => {
-    console.log('New Connection!');
-    ws.on('message', (msg) => {
-        msg = msg.toString('utf-8')
-        try {
-            msgData = JSON.parse(msg);
-            console.log('Received JSON: ' + msgData)
-            addUser(msgData.sender, ws)
-            addConversation(msgData.conversation_id, msgData.sender, msgData.receiver);
-        } catch (e) {
-            console.log('Received non-JSON: ' + msg);
-        }
-    });
+server.on('upgrade', (request, socket, head) => {
+    // TODO: message-server & presence-server seperation
 });
+
+wss.on('connection', (ws, req) => {
+    logger.info('New connection received!');
+    registerCallbacks(ws);
+});
+
+function routeMessage(msgData, sender) {
+    const participants = getConvParticipants(msgData.conversationId);
+    if (!participants) {
+        logger.error(`No participants found for conv id: ${msgData.conversationId}`);
+        return;
+    }
+    
+    delete msgData.to;
+    msgData.from = sender;
+
+    for (let user of participants) {
+        if (user === sender) continue;
+
+        let ws = getUserSocket(user);
+        if (!ws) {
+            logger.debug(`Socket not found for user: ${user}`);
+            continue;
+        };
+        logger.info(`Routing message (${msgData.messageId}) to (${user})`);
+        ws.send(JSON.stringify(msgData));
+    }
+}
+
+function handleMessage(msg, ws) {
+    try {
+        data = parser.parse(msg);        
+    } catch (e) {
+        logger.warn(`Received non-JSON: ${msg.toString()}`);
+    }
+
+    addConversation(data.conversationId, ws.userId, data.to);
+    routeMessage(data, ws.userId);
+}
+
+// TODO: user authentication; dynamodb
+function authenticate(data) {
+    return true;
+}
+
+function registerCallbacks(ws) {
+    ws.on('message', (msg) => {
+        if (!ws.authenticated) {
+            ws.authenticated = true;
+            data = JSON.parse(msg);
+            if (authenticate(data)) {
+                logger.info(`User ${data.userId} authenticated!`);
+                addUser(data.userId, ws);
+                ws.userId = data.userId; // TODO: set user id from received data
+            } else {
+                ws.close();
+            }
+
+            return;
+        }
+
+        handleMessage(msg, ws);
+    });
+
+    ws.on('close', () => {
+        logger.info(`Connection closed for: ${ws.userId}`);
+        removeUser(ws.userId);
+    });
+}
 
 server.listen(443);
